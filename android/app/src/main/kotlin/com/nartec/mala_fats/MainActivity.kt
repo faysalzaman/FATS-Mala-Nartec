f0a58f36527fff7af479c84ae4f2916e2af13b71
Faysal Zaman <123411654+faysalzaman@users.noreply.github.com>
2025-02-13 18:37:04 +0500

    Created a new project - main branch

---
+++ android/app/src/main/kotlin/com/nartec/mala_fats/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nartec.mala_fats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mala_fats/screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mala Fats',
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
